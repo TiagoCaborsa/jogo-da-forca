8305c194a0d75885c2cfe9bd0ed8654eb2ee343b
TiagoCaborsa <tiago.gnr1985@gmail.com>
2018-11-28 02:04:38 -0200

    Adds ClienteControle.

---
+++ multiplayer/src/cliente/ClienteControle.kt
@@ -0,0 +1,32 @@
+package cliente
+
+import servidor.ServidorConfig
+import java.io.ObjectOutputStream
+import java.net.Socket
+import java.util.*
+
+class ClienteControle(private val servidorConfig: ServidorConfig) {
+
+    fun iniciarCliente() {
+        try {
+
+            val socket = Socket(servidorConfig.ip, servidorConfig.porta)
+            println("Conectado ao servidor.")
+            val outputStream = ObjectOutputStream(socket.getOutputStream())
+
+            val scanner = Scanner(System.`in`)
+
+            while (true) {
+                println("Digite a msg!")
+                outputStream.writeObject(scanner.nextLine())
+                outputStream.flush()
+            }
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+            System.err.println("Erro ao iniciar cliente: ${e.message}")
+        }
+
+    }
+
+}
